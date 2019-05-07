e5116ad7ff1153d515833ab747cae4e0daf35441
Ryan Yim <yknyim@gmail.com>
2019-05-07 09:50:24 -0400

    adds skeleton of redux code with pokemon deck

--- src/index.js
+++ src/index.js
@@ -4,6 +4,50 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// =====================================================
+// STATE
+
+import initialState from './base.json';
+import { identifier } from '@babel/types';
+
+// the state is an object with a cards property which is an array of objects
+// { cards: [ {}, {}, {}, {} ] }
+
+// =====================================================
+// ACTIONS + ACTIONS CREATORS
+const ACTION_CATCH = 'catch';
+
+function catchCard(id) {
+    return {
+        type: ACTION_CATCH,
+        payload: {
+            id,
+        }
+    }
+}
+
+// =====================================================
+// REDUCER
+
+function cards(state=initialState, action={type: ''}) {
+    
+    switch(action.type) {
+        case ACTION_CATCH:
+            // find the card, set it to "caught"
+        break;
+        default:
+            return state;
+        break;
+    }
+}
+
+// =====================================================
+// STORE
+
+
+
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
